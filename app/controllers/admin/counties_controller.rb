cbad0b27912facd93d0e6074572c97095d170b44
Alessandro <alessandro@alessandro-Aspire-5750.(none)>
2013-11-02 16:59:30 -0200

    Criacao da action create no countiescotroller

--- app/controllers/admin/counties_controller.rb
+++ app/controllers/admin/counties_controller.rb
@@ -1,2 +1,19 @@
 class Admin::CountiesController < Admin::AdminController
+	def index 
+		@counties = County.all
+	end
+
+	def edit
+		@county = County.find( params[:id] )
+	end
+
+	def new
+		@county = County.new
+	end
+
+	def create
+		@county = County.new( params.require("county").permit(:name, :code, :cnpj) )
+		@county.save
+		redirect_to admin_counties_path, notice: "salvo com sucesso"
+	end
 end
--- spec/controllers/admin/counties_controller_spec.rb
+++ spec/controllers/admin/counties_controller_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper' 
 
-describe Admin::CountiesController do 
-	
+describe Admin::CountiesController do
+	let(:county){ County.create name: "Test", code: "123", cnpj: "123456" }
+
+	describe "GET #index" do
+		before :each do
+			get :index
+		end
+
+		it { should respond_with(:success) }
+		
+		it "should respond a new list of counties" do
+			assigns(:counties).should =~ [ county ]
+		end
+	end
+
+	describe "GET edit" do
+		before(:each){ get :edit, id: county.id }
+
+		it { should respond_with(:success) }
+		it { should render_template(:edit) }
+
+		it "should respond to a register" do
+			assigns(:county).should eq( county )
+		end
+	end
+
+	describe "GET new" do
+		before(:each){ get :new }
+
+		it { should respond_with(:success) }
+		it { should render_template(:new) }
+
+		it "should respond to a register" do
+			assigns(:county).new_record?.should be_true
+		end
+	end
+
+	describe "POST create" do
+		before(:each){ post :create, county: { name: "Bolas", code: "123", cnpj: "1234" }  }
+
+		it { should respond_with(:redirect) }
+		it { should redirect_to( admin_counties_path ) }
+    	it { should set_the_flash.to( "salvo com sucesso" ) }
+
+	end	
+  
 end
